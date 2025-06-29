cf0625b6cb16f7d9dfac6ea34d552bf610cd8243
pacoblack <pacoson.wang@gmail.com>
2025-06-29 22:21:26 +0800

    修复全屏问题

--- exo-video-play/src/main/java/com/cv/pic/exo/video/VideoPlayerActivity.kt
+++ exo-video-play/src/main/java/com/cv/pic/exo/video/VideoPlayerActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -39,6 +38,7 @@ class VideoPlayerActivity : AppCompatActivity() {
   private lateinit var progressBar: ProgressBar
   private lateinit var player: ExoPlayer
   private lateinit var videoCache: Cache
+  private var isFullscreen = true
 
   @OptIn(UnstableApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +47,8 @@ class VideoPlayerActivity : AppCompatActivity() {
 
     playerView = binding.playerView
     progressBar = binding.progressBar
-
+    playerView.setFullscreenButtonClickListener {toggleFullscreen(!isFullscreen)}
+    toggleFullscreen(false)
     // 初始化缓存目录
     val cacheDir = getCacheDirectory(this)
     if (cacheDir != null) {
@@ -58,6 +59,51 @@ class VideoPlayerActivity : AppCompatActivity() {
     } else {
       finish()
     }
+  }
+
+  private fun toggleFullscreen(value:Boolean) {
+    isFullscreen = value
+
+    if (isFullscreen) {
+      enterFullscreen()
+    } else {
+      exitFullscreen()
+    }
+  }
+
+  private fun enterFullscreen() {
+    // 1. 隐藏系统UI
+    hideSystemUI()
+
+    // 2. 锁定横屏
+    requestedOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+  }
+
+  private fun exitFullscreen() {
+    // 1. 显示系统UI
+    showSystemUI()
+
+    // 2. 解锁屏幕方向
+    requestedOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+  }
+
+  private fun hideSystemUI() {
+    window.decorView.systemUiVisibility = (
+      View.SYSTEM_UI_FLAG_FULLSCREEN
+        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      )
+  }
+
+  private fun showSystemUI() {
+    window.decorView.systemUiVisibility = (
+      View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      )
   }
 
   override fun onStart() {
