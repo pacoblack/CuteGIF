88dcdfa5d8629fd802fe6d5f98b978cc19ebac95
paco <pacoson.wang@gmail.com>
2025-07-03 09:36:17 +0800

    优化图片预览代码

--- app/src/main/java/me/tasy5kg/cutegif/activity/ImageActivity.kt
+++ app/src/main/java/me/tasy5kg/cutegif/activity/ImageActivity.kt
@@ -1,54 +1,45 @@
 package me.tasy5kg.cutegif.activity
 
-import android.R
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.target.Target
+import me.tasy5kg.cutegif.R
 import me.tasy5kg.cutegif.databinding.ActivityImageBinding
 import me.tasy5kg.cutegif.databinding.ItemImagePreviewBinding
-import me.tasy5kg.cutegif.databinding.ItemMergePageBinding
+import me.tasy5kg.cutegif.toolbox.Toolbox.toast
 
 class ImagePreviewActivity : AppCompatActivity() {
   private val binding by lazy { ActivityImageBinding.inflate(layoutInflater) }
 
   private lateinit var viewPager: ViewPager2
   private lateinit var adapter: ImageAdapter
-  private lateinit var indicatorContainer: LinearLayout
-  private lateinit var tvTitle: TextView
 
-  // 指示器点
-  private val indicators: MutableList<ImageView?> = ArrayList<ImageView?>()
-
-  // 图片资源（实际项目中可以从Intent传递）
-  private var imageUrls: MutableList<String?>? = ArrayList<String?>()
+  private val imageUrls by lazy { intent.getStringArrayListExtra("image_urls")  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
 
-
-    // 获取传递的图片列表和初始位置
-    imageUrls = intent.getStringArrayListExtra("image_urls")
     val startPosition = intent.getIntExtra("start_position", 0)
-
+    if (imageUrls.isNullOrEmpty()) {
+      toast("图片列表为空，请重新选择")
+      finish()
+      return
+    }
+    // 关闭按钮
+    binding.btnClose.setOnClickListener { finish() }
 
     // 初始化视图
     viewPager = binding.viewPager
-    tvTitle = binding.tvTitle
 
     // 设置适配器
     adapter = ImageAdapter(this, imageUrls!!)
@@ -66,28 +57,18 @@ class ImagePreviewActivity : AppCompatActivity() {
       override fun onPageSelected(position: Int) {
         super.onPageSelected(position)
         updateTitle(position)
-        updateIndicators(position)
       }
     })
 
-    // 关闭按钮
-    val btnClose = binding.btnClose
-    btnClose.setOnClickListener(View.OnClickListener { v: View? -> finish() })
   }
 
   private fun updateTitle(position: Int) {
-    tvTitle!!.setText(String.format("图片预览 (%d/%d)", position + 1, imageUrls!!.size))
-  }
-
-  private fun updateIndicators(position: Int) {
-    for (i in indicators.indices) {
-      indicators.get(i)!!.setSelected(i == position)
-    }
+    binding.tvTitle.text = String.format("图片预览 (%d/%d)", position + 1, imageUrls!!.size)
   }
 
   // 图片适配器
-  internal class ImageAdapter(val context:Context, val binding: ItemImagePreviewBinding, private val imageUrls: MutableList<String?>) :
-    RecyclerView.Adapter<ImageAdapter.ImageViewHolder?>() {
+  internal class ImageAdapter(val context:Context, private val imageUrls: ArrayList<String>) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
       return ImageViewHolder(ItemImagePreviewBinding.inflate(
         LayoutInflater.from(parent.context),
@@ -99,9 +80,9 @@ class ImagePreviewActivity : AppCompatActivity() {
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
       // 使用Glide加载图片（支持网络图片和本地资源）
       Glide.with(context)
-        .load(imageUrls[position])
-        .placeholder(R.drawable.ic_secure)
-        .error(R.drawable.ic_secure)
+        .load(imageUrls)
+        .placeholder(R.drawable.media_placeholder)
+        .error(R.drawable.media_placeholder)
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(holder.photoView)
     }
@@ -117,7 +98,7 @@ class ImagePreviewActivity : AppCompatActivity() {
 
   companion object {
     // 启动预览Activity的方法
-    fun start(context: Context, imageUrls: ArrayList<String?>?, startPosition: Int) {
+    fun start(context: Context, imageUrls: ArrayList<String>, startPosition: Int) {
       val intent = Intent(context, ImagePreviewActivity::class.java)
       intent.putStringArrayListExtra("image_urls", imageUrls)
       intent.putExtra("start_position", startPosition)
